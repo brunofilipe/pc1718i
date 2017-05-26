c68de2a72ad6d28b348edd7ea094f044afcc55fe
brunofilipe <bruno.filipe1996@hotmail.com>
2017-05-26 17:03:49 +0100

    implemented void Leave function

--- Serie1/Serie1/ThrottledRegion.cs
+++ Serie1/Serie1/ThrottledRegion.cs
@@ -1,32 +1,63 @@
 ﻿using System.Collections.Generic;
 
 namespace Serie1 {
-    public class ThrottledRegion {
-
-        private readonly int maxInside;
+     private readonly int maxInside;
         private readonly int maxWaiting;
         private readonly int waitTimeout;
-        private LinkedList<int> queue;
+        private readonly Dictionary<int, RegionObj> regionThreads;
 
         public ThrottledRegion(int maxInside, int maxWaiting, int waitTimeout) {
             this.maxInside = maxInside;
             this.maxWaiting = maxWaiting;
             this.waitTimeout = waitTimeout;
-            queue = new LinkedList<int>();
+            regionThreads = new Dictionary<int, RegionObj>();
         }
 
-        public bool TryEnter(int key) {// throws ThreadInterruptedException
+        public bool TryEnter(int key) {
             lock (this) {
+                if (!regionThreads.ContainsKey(key)) {
+                    RegionObj obj = new RegionObj(maxInside, maxWaiting);
+                    regionThreads.Add(key, obj);
+                }
+                RegionObj regionThread = regionThreads[key];
+                if (regionThread.permits > 0) {
+                    regionThread.permits--;
+                    return true;
+                }
+                //if thread doesn't have permits , we must checked if it reached timeout
                 if (waitTimeout == 0) {
                     return false;
                 }
-            }
+                int lastTime = (waitTimeout != Timeout.Infinite) ? Environment.TickCount : 0;
+                do {
+                    try {
+                        Monitor.Wait(this, waitTimeout);
+                    }catch (ThreadInterruptedException) {
+                        if (regionThread.permits > 0) {
+                            Monitor.PulseAll(regionThread);
+                        }
+                        throw;
+                    }
 
-            return false;
+                   
+                } while (true);
+
+            }
         }
 
         public void Leave(int key) {
-            
-        }
+            lock (this) {
+                RegionObj regionThread = regionThreads[key];
+                if (regionThread == null) {
+                    throw new Exception("There is no region with this key");
+                }
+                if (regionThread.permits < maxInside) { //if the region still has permits
+                    regionThread.permits++;
+                    Monitor.PulseAll(regionThread);
+                }
+                throw new Exception("The region doesn´t support more accesses");
+            }
+        }
+
     }
 }
