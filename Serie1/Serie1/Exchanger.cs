3cbd123d8b7663a64524f0fb069c875ca6369aef
brunofilipe <bruno.filipe1996@hotmail.com>
2017-05-28 22:32:44 +0100

    started exercise 2

---
+++ Serie1/Serie1/Exchanger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Serie1 {
+    public class Exchanger<T> where T : class {
+        public T Exchange(T mine, int timeout) {
+            lock (this) {
+                int lastTime = (timeout != Timeout.Infinite) ? Environment.TickCount : 0;
+                do {
+                    try {
+                        Monitor.Wait(this, timeout);
+                    }
+                    catch (ThreadInterruptedException) {
+                        
+                        throw;
+                    }
+                    if (SyncUtils.AdjustTimeout(ref lastTime, ref timeout) == 0)
+                    {
+                        Monitor.PulseAll(this);
+                        return null;
+                    }
+                } while (true);
+            }
+        }
+
+    }
+}
