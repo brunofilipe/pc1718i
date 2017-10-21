b704a7c8b6748e3e18ca7a2257e17c85b1c70dd5
brunofilipe <bruno.filipe1996@hotmail.com>
2017-10-21 17:51:23 +0100

    started serie1 for 1718 winter

---
+++ pc1718i/ConcurrencyProgramming/serie1/ExpirableLazy.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConcurrencyProgramming.serie1 {
+    public class ExpirableLazy<T> where T : class {
+
+        private readonly Func<T> provider;
+        private bool isCompleted;
+        private T val;
+        private readonly TimeSpan timeToLive;
+        private readonly object _lock = new object();
+        private LinkedList<int> threadQueue;
+
+        public ExpirableLazy(Func<T> provider, TimeSpan timeToLive) {
+            this.provider = provider;
+            this.timeToLive = timeToLive;
+            this.threadQueue = new LinkedList<int>();
+        }
+
+        public T Value {
+            get {
+                lock (_lock) {
+                    int threadId = Thread.CurrentThread.ManagedThreadId;
+                    TimeSpan now = DateTime.Now.TimeOfDay;
+                    if (timeToLive >= now && isCompleted) {
+                        return val;
+                    }
+                    //fazer o calculo, e retornar o val , tudo na thread invocante
+                    if ((!isCompleted || timeToLive < now) && threadQueue.Count == 0) {
+                        try {
+                            val = provider();
+                            isCompleted = true;
+                            return val;
+                        }
+                        catch (Exception) {
+                            throw;
+                        }
+                    }
+                    LinkedListNode<int> node = threadQueue.AddLast(threadId);
+                    do {
+                        try {
+                            Monitor.Wait(_lock);
+                        }
+                        catch (ThreadInterruptedException) {
+                            threadQueue.Remove(node);
+                            Monitor.PulseAll(_lock);
+                            throw;
+                        }
+
+                        if (val == null && threadQueue.First.Value == threadId) {
+                            threadQueue.Remove(node);
+                            try {
+                                val = provider();
+                                isCompleted = true;
+                                return val;
+                            }
+                            catch (Exception) {
+                                Monitor.PulseAll(_lock);
+                                throw;
+                            }
+                        }
+                        if (val != null){
+                            Monitor.PulseAll(_lock);
+                            threadQueue.Remove(node);
+                            return val;
+                        }
+
+                    } while (true);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ pc1718i/Tests/setie1Tets/ExpirableLazyTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ConcurrencyProgramming.serie1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.setie1Tets {
+    [TestClass]
+    public class ExpirableLazyTests {
+        private ExpirableLazy<string> exLazy;
+
+        [TestMethod]
+        public void TestSingleItemExpirable() {
+            exLazy = new ExpirableLazy<string>(()=>"single result",new TimeSpan(1,1,1));
+            Assert.AreEqual("single result",exLazy.Value);
+        }
+
+        [TestMethod]
+        public void TestAfterTimeSpan() {
+            exLazy = new ExpirableLazy<string>(() => "single result", new TimeSpan(0, 0, 0));
+            Assert.AreEqual("single result", exLazy.Value);
+        }
+    }
+}
