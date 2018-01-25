013769c7f3840a7dd9cb244f22c922e04f6c456a
brunofilipe <bruno.filipe1996@hotmail.com>
2018-01-25 15:07:28 +0000

    starting ex3

---
+++ pc1718i/ConcurrencyProgramming/serie3/FileSearcher/Search.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrencyProgramming.serie3.FileSearcher {
+    class Search {
+    }
+}
