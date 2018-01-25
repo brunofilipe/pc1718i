de4404374597fd4b0e1f84d06529af5c86cf82fd
Nunobpinto <nunobernardopinto@gmail.com>
2018-01-25 19:14:10 +0000

    begun exercise3

--- pc1718i/ConcurrencyProgramming/serie3/FileSearcher/ResultContainer.cs
+++ pc1718i/ConcurrencyProgramming/serie3/FileSearcher/ResultContainer.cs
@@ -9,31 +9,42 @@ using System.IO;
 
 namespace ConcurrencyProgramming.serie3.FileSearcher {
     class ResultContainer {
-        private  ConcurrentQueue<FileInfo> queue;
-        private volatile int filesCount;
         private readonly int MAX_SIZE;
-
+        private object _lock = new object();
+        private List<FileInfo> _queue;
+        //private ConcurrentQueue<FileInfo> _queue;
+        private FileInfo smallestFile;
+        public int FilesCount { get; set; }
+        
         public ResultContainer(int maxSize) {
-            queue = new ConcurrentQueue<FileInfo>();
+            //_queue = new ConcurrentQueue<FileInfo>();
+            _queue = new List<FileInfo>();
             MAX_SIZE = maxSize;
         }
 
         public List<string> GetFiles() {
-            return queue.Select(file => file.FullName).ToList();
+            return _queue.Select(file => file.FullName).ToList();
         }
 
         public int GetFilesFound() {
-            return filesCount;
+            return FilesCount;
         }
 
         public void TryAddFile(FileInfo file) {
-            Interlocked.Increment(ref filesCount);
+            lock (_lock) {
+                if (_queue.Count < MAX_SIZE)
+                    _queue.Add(file);
+                else {
+                    _queue.Remove(_queue[_queue.Count-1]);
+                    _queue.Sort( (info, fileInfo) => (int) (file.Length - fileInfo.Length));
+                   
+                }
+            }
             //add File...
             //sorting elements in queue
-            var list = queue.ToList();
-            list = list.OrderBy(f => f.Length).ToList();
-            queue = new ConcurrentQueue<FileInfo>(list);
-            throw new NotImplementedException();
+            //var list = _queue.ToList();
+            //list = list.OrderBy(f => f.Length).ToList();
+            //_queue = new ConcurrentQueue<FileInfo>(list);
         }
     }
 }
--- pc1718i/ConcurrencyProgramming/serie3/FileSearcher/Search.cs
+++ pc1718i/ConcurrencyProgramming/serie3/FileSearcher/Search.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConcurrencyProgramming.serie3.FileSearcher {
     class Search {
+        public static ResultContainer ParallelGetBiggestFiles(string path, int numOfFiles, CancellationToken token) {
+            if (!Directory.Exists(path)) {
+                throw new ArgumentException();
+            }
+            token.ThrowIfCancellationRequested();
+
+            var result = new ResultContainer(numOfFiles);
+            var files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            result.FilesCount = files.Length;
+
+            Parallel.ForEach(files, (filePath, loopState) => {
+                if (token.IsCancellationRequested)
+                    loopState.Stop();
+
+                var file = new FileInfo(filePath);
+                result.TryAddFile(file);
+            });
+
+            token.ThrowIfCancellationRequested();
+            return result;
+        }
     }
 }
